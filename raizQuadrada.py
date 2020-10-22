472afb52bd7ced8e091676eb2f1a1193ec9702e5
Luan Teixeira <luanlts@outlook.com>
2020-10-22 12:55:53 -0300

    LUAN: Refatoração bhaskara.py [FINALIZADO]

--- raizQuadrada.py
+++ raizQuadrada.py
@@ -5,14 +5,12 @@ c = int(input("Digite o valor para C: \n"))
 
 delta = b ** 2 - 4 * a * c
 
-if delta == 0:
-    x = (-b + math.sqrt(delta)) / (2 * a)
-    print("A equação possui apenas um raíz real X =", x)
-
-elif delta > 0:
+if delta < 0:
+    print("A equação não possui raízes reais.")
+else:
     x1 = (-b + math.sqrt(delta)) / (2 * a)
     x2 = (-b - math.sqrt(delta)) / (2 * a)
-    print("A equação possui duas raízes X1 =", x1, "e X2 =", x2)
-
-else:
-    print("A equação não possui raízes reais.")
+    if delta == 0:
+        print("A equação possui apenas um raíz real X =", x1)
+    else:        
+        print("A equação possui duas raízes X1 =", x1, "e X2 =", x2)
