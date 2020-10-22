e4d47da1ec18defe9dcedd054681ed1efc4a7703
Luan Teixeira <luanlts@outlook.com>
2020-10-22 12:04:22 -0300

    LUAN: alterações na fórmula de Bhaskara [FINALIZADO]

--- raizQuadrada.py
+++ raizQuadrada.py
@@ -3,15 +3,15 @@ a = int(input("Digite o valor para A: \n"))
 b = int(input("Digite o valor para B: \n"))
 c = int(input("Digite o valor para C: \n"))
 
-delta = b**2 - (4 * a * c)
+delta = b ** 2 - 4 * a * c
 
 if delta == 0:
-    x = ((-b) + math.sqrt(delta)) / (2 * a)
-    print("A equação possui apenas um raíz real X =", delta)
+    x = (-b + math.sqrt(delta)) / (2 * a)
+    print("A equação possui apenas um raíz real X =", x)
 
 elif delta > 0:
-    x1 = ((-b) + math.sqrt(delta)) / 2 * a
-    x2 = ((-b) - math.sqrt(delta)) / 2 * a
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
     print("A equação possui duas raízes X1 =", x1, "e X2 =", x2)
 
 else:
