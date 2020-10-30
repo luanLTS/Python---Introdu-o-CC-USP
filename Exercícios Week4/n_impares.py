d5a67b526702a109a1844474b1c24adeb1ad3ed1
Luan Teixeira <luanlts@outlook.com>
2020-10-29 21:34:08 -0300

    LUAN: exercicios semana 4 [FINALIZADO]

---
+++ Exercícios Week4/n_impares.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o valor de n: "))
+i = 1
+
+while (i <= n*2):
+    if i % 2 == 1:
+        print(i)
+    i+=1
---
+++ Exercícios Week4/soma_digitos.py
@@ -0,0 +1,16 @@
+n = int(input("Digite um número inteiro: "))
+aux = len(str(n))
+i = 1
+soma = 0
+    
+while i <= aux:
+    ultDig = n % 10
+    soma = soma + ultDig
+    n = n // 10
+    i+=1
+print(soma)
+'''
+x = 123
+print(x//10) 
+print(x%10)
+'''
