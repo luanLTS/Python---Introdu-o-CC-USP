a32e81b23f333588570189a7751a8b3b785bfe33
Luan Teixeira <luanlts@outlook.com>
2020-11-01 15:22:06 -0300

    LUAN: alterando ordenacao.py

--- Exercícios Week3/ordenacao.py
+++ Exercícios Week3/ordenacao.py
@@ -2,7 +2,7 @@ n1 = int(input("Digite um inteiro: \n"))
 n2 = int(input("Digite um inteiro: \n"))
 n3 = int(input("Digite um inteiro: \n"))
 
-if n1 < n2 and n2 < n3:
+if n1 < n2 < n3:
     print("crescente")
 else:
     print("não está em ordem crescente")
