362c44708e720e6e4a80cb09698a7a77c1ae416e
Luan Teixeira <luanlts@outlook.com>
2020-11-06 12:09:20 -0300

    LUAN: refatorando o código bhaskara.py utilizando funções, bhaskaraFuncoes.py

--- bhaskaraFuncoes.py
+++ bhaskaraFuncoes.py
@@ -1,7 +1,7 @@
 import math
 
 def main():
-    bhaskara(10, -1, 1)
+    bhaskara(-9, 15, 5)
 
 def bhaskara (a, b, c):    
     delta(a, b, c)        
@@ -9,16 +9,16 @@ def bhaskara (a, b, c):
 def delta(a, b, c):
     d = b ** 2 - 4 * a * c
     if d < 0:
-        return "A equação não possui raízes reais."
+        print("A equação não possui raízes reais.") 
     else:
-        raizes(a, b, delta)
+        raizes(a, b, d)
 
-def raizes(a, b, delta):
-    x1 = (-b + math.sqrt(delta)) / (2 * a)
-    x2 = (-b - math.sqrt(delta)) / (2 * a)
+def raizes(a, b, d):
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
     if delta == 0:
-        return f"A equação possui apenas um raíz real X = {x1}"
+        print(f"A equação possui apenas um raíz real X = {x1}")
     else:
-        return f"A equação possui duas raízes X1 = {x1} e X2 = {x2}"
+        print(f"A equação possui duas raízes X1 = {x1} e X2 = {x2}") 
 
-main()  
+main()
